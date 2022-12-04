c2445f82eb0c7bd0b2c26139a715440557dc7ba5
IkuraKoutaro <nov.12.kou@outlook.jp>
2022-12-04 16:34:03 +0900

    fail

--- voronoi_7.py
+++ voronoi_7.py
@@ -5,6 +5,7 @@ import numpy as np
 from matplotlib.collections import LineCollection
 from scipy._lib.decorator import decorator as _decorator
 from scipy.spatial import Voronoi, voronoi_plot_2d
+import matplotlib.animation as anm
 
 class Voro:
     def __init__(self):
@@ -17,15 +18,6 @@ class Voro:
         xy_max = points.max(axis=0) + margin
         ax.set_xlim(xy_min[0], xy_max[0])
         ax.set_ylim(xy_min[1], xy_max[1])
-
-    # def delaunay_plot(self, tri, ax=None):
-    #     if tri.points.shape[1] != 2:
-    #         raise ValueError("Delaunay triangulation is not 2-D")
-    #     x, y = tri.points.T
-    #     ax.plot(x, y, 'o')
-    #     ax.triplot(x, y, tri.simplices.copy())
-    #     self._adjust_bound(ax, tri.points)
-    #     return ax.figure
 
     def convex_hull_plot(self, hull, ax=None):
         if hull.points.shape[1] != 2:
@@ -56,9 +48,9 @@ class Voro:
         # numpy.ptp : Range of values (maximum - minimum) along an axis.
         ptp_bound = vor.points.ptp(axis=0)
 
-        print('vor.points : ', vor.points)
-        print('center : ', center)
-        print('ptp_bound : ', ptp_bound)
+        # print('vor.points : ', vor.points)
+        # print('center : ', center)
+        # print('ptp_bound : ', ptp_bound)
 
         finite_segments = []
         infinite_segments = []
@@ -147,10 +139,10 @@ class Voro:
                 # print('vor.vertices[i]+0.2 :', vor.vertices[i]+0.2)
                 # print('ptp_bound.max() : ', ptp_bound.max())
                 # print('far_point', far_point)
-                print('=================================================================')
+                #print('=================================================================')
 
-        ax.add_collection(LineCollection(finite_segments, colors=line_colors , lw=1,alpha=line_alpha,linestyle='solid'))
-        ax.add_collection(LineCollection(infinite_segments,colors=line_colors, lw=1,alpha=line_alpha,linestyle='solid'))
+        ax.add_collection(LineCollection(finite_segments, colors=line_colors,lw=1,alpha=line_alpha,linestyle='solid'))
+        ax.add_collection(LineCollection(infinite_segments,colors=line_colors,lw=1,alpha=line_alpha,linestyle='solid'))
 
         self._adjust_bound(ax, vor.points)
 
@@ -178,47 +170,80 @@ class World:
         voronoi = Voro()
         self.points_robot = voronoi.draw_voronoi(ax)
 
-        # points_robot : location of robots
-        print('robot location', self.points_robot)
-        #appendの引数objはIdealRobotのインスタンスであるから
-        #obj.drawはClass IdealRobotのdraw関数である.
-        for obj in self.objects:    #appendした物体を次々に描画
-            obj.robot_draw(ax, self.points_robot, self.count)
-            self.count+=1
-
-        #axの調整
-        ax.set_aspect('equal')    #縦横比を座標の値と一致させる
-        ax.set_xlim(0,10)         #X軸を-5m×5mの範囲で描画
-        ax.set_ylim(0,10)         #Y軸も同様に
+        ax.set_aspect('equal')    
+        ax.set_xlim(0,10)
+        ax.set_ylim(0,10)
         ax.set_xlabel("X",fontsize=20)
         ax.set_ylabel("Y",fontsize=20)
 
+        #描画する図形のリスト
+        elems = []
+        self.ani = anm.FuncAnimation(fig, self.world_one_step, fargs=(elems,ax), frames=40, interval=1000, repeat=False)
+        #fargs : one_step(func)に渡す引数
+        #one_stepの引数i(ステップの番号)は自動的に渡るので渡す必要はない
         plt.show()
 
+    def world_one_step(self, i, elems, ax):
+        while elems:elems.pop().remove()
+        elems.append(ax.text(-4.4,4.5,"t="+str(i),fontsize=10))
+        for obj in self.objects:    #appendした物体を次々に描画
+            
+            obj.robot_draw(ax, self.points_robot, self.count, elems)
+            self.count+=1
+            if hasattr(obj, "world_one_step"): obj.world_one_step(1.0)
+
 class IdealRobot:
-    def __init__(self, pose, color="black"):
+    def __init__(self, pose, agent=None, color="black"):
         self.pose = pose
         self.r = 0.2
+        self.agent = agent
         self.color = color
+        self.poses = [pose]  #軌跡の描画用
     
-    def robot_draw(self, ax, points_robot, i):
-        # print(i)
+    def robot_draw(self, ax, points_robot, i, elems):
         x, y = points_robot[i]
         theta =  math.pi/6
         xn = x + self.r * math.cos(theta)
         yn = y + self.r * math.sin(theta)
-
-        ax.plot([x,xn], [y,yn], color=self.color)
+        elems += ax.plot([x,xn],[y,yn],color=self.color)  #ロボットの向きを示す線分の描画 #x,yは円の中心 #xからxnに向けて線を引く #yも同様
         c = patches.Circle(xy=(x,y), radius=self.r, fill=False, color=self.color)
+        elems.append(ax.add_patch(c))
 
-        ax.add_patch(c)
+        self.poses.append(self.pose)
+        elems += ax.plot([e[0] for e in self.poses], [e[1] for e in self.poses], linewidth=0.5, color="black")
+    
+    #引数で与えられてたposeに姿勢の変化量を足して,移動後の姿勢を返す関数
+    @classmethod  #オブジェクトを作らなくてもメソッドを実行できるようにするため
+    def state_transition(cls, nu, omega, time, pose):
+        t0 = pose[2]
+        if math.fabs(omega) < 1e-10:   #角速度がほぼゼロの場合とそうでない場合で場合分け
+            return pose + np.array([nu*math.cos(t0), nu*math.sin(t0), omega]) * time
+        else:
+            return pose+np.array([nu/omega*(math.sin(t0+omega*time)-math.sin(t0)), nu/omega*(-math.cos(t0+omega*time)+math.cos(t0)), omega*time])
+    
+    #one_stepはエージェントが存在する場合にはエージェントからロボットの速度,角速度を受け取り,姿勢を更新する処理を行う
+    #time_interval:離散時間1ステップが何秒であるかをしているするもの
+    def idealRobot_one_step(self, time_interval):
+        if not self.agent:return
+        nu, omega = self.agent.decision()
+        self.pose = self.state_transition(nu, omega,time_interval, self.pose)
+
+class Agent:
+    def __init__(self, nu, omega):
+        self.nu = nu
+        self.omega = omega
+    def decision(self, observation=None):
+        return self.nu, self.omega
+
 
 world = World()
+straight = Agent(0.2,0.0)                 #0.2[m/s]で直進
+circling = Agent(0.2,10.0/180*math.pi)    #0.2[m/s], 10[deg/s]（円を描く）
 #np.array([x,y,theta])であり、poseに参照される
 #.Tは転置
 
-robot1 = IdealRobot( np.array([2,3]).T)          #ロボットのインスタンスを生成
-robot2 = IdealRobot( np.array([8,9]).T, "red")   #ロボットのインスタンスを生成（赤）
+robot1 = IdealRobot( np.array([2,3]).T, straight)          #ロボットのインスタンスを生成
+robot2 = IdealRobot( np.array([8,9]).T, circling, "red")   #ロボットのインスタンスを生成（赤）
 robot3 = IdealRobot( np.array([8,9]).T, "blue")
 robot4 = IdealRobot( np.array([8,9]).T, "green")
 robot5 = IdealRobot( np.array([8,9]).T, "orange")
